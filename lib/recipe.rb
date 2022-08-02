87f0007b823b15a439781bc286ac3754b8dff810
Riley McCullough <rileybmccullough@gmail.com>
2022-08-02 10:18:12 -0600

    Feat: can return total calories in a recipe

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -13,7 +13,16 @@ class Recipe
 
   def ingredients
     @ingredients_required.keys
-    # @ingredients_required.keys.map { |food| food.name } #I think you wanted the ingredient instance, but this will return legible names
+    # @ingredients_required.keys.map { |food| food.name }
+    #I think you wanted the ingredient instance, but this will return legible names
+  end
+
+  def total_calories
+    numz = 0
+    @ingredients_required.keys.each do |key|
+      numz += (key.calories * @ingredients_required[key] )
+    end
+    numz
   end
 
 end
