a5ea0896686495b0fc616f2ab7f8664b05aeab29
Riley McCullough <rileybmccullough@gmail.com>
2022-08-02 10:59:24 -0600

    Feat: can return ingredients for a cookbook

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -9,5 +9,16 @@ class CookBook
     @recipes << thing
   end
 
+  def ingredients
+    shopping_list = []
+    @recipes.each do |dish|
+      dish.ingredients.each do |item|
+        shopping_list << item.name
+      end
+    end
+    shopping_list.uniq
+  end
+
+
 
 end
