7650fc8209086c6369e7ad94021ddd2d7d79ef9d
Riley McCullough <rileybmccullough@gmail.com>
2022-08-02 09:19:31 -0600

    Fix: spelling error in test

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -1,22 +1,20 @@
-require ' ./lib/ingredient'
-RSpec.decribe Ingredient do
+require './lib/ingredient'
+RSpec.describe Ingredient do
 
   before :each do
-
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-
   end
 
   it 'can return ingredient name' do
-    expect(ingredient1.name).to eq("Cheese")
+    expect(@ingredient1.name).to eq("Cheese")
   end
 
   xit 'can retun unit of measure for ingredient' do
-    expect(ingredient1.unit).to eq("oz")
+    expect(@ingredient1.unit).to eq("oz")
   end
 
   xit 'can return calories in ingredient' do
-    expect(ingredient1.calories).to eq(50)
+    expect(@ingredient1.calories).to eq(50)
   end
-  
+
 end
